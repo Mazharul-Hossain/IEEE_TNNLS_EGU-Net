b6f7993a9950dd4fb87386f30617c5b7e53a3e51
Mazhar <2561458+Mazharul-Hossain@users.noreply.github.com>
2022-09-21 21:20:19 -0500

    Update EGU-Net-ss.py

--- EGU-Net-ss.py
+++ EGU-Net-ss.py
@@ -214,8 +214,8 @@ def my_network_optimization(y_est, y_re, r1, r2, l2_loss, reg, learning_rate, gl
         # https://github.com/tensorflow/docs/blob/r1.14/site/en/api_docs/python/tf/train/Optimizer.md
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         gradients, variables = zip(*optimizer.compute_gradients(cost))
-        # gradients, _ = tf.clip_by_global_norm(gradients, 0.001)
-        gradients = [tf.clip_by_norm(gradient, 0.001) for gradient in gradients]
+        gradients, _ = tf.clip_by_global_norm(gradients, 0.001)
+        # gradients = [tf.clip_by_norm(gradient, 0.001) for gradient in gradients]
         for g, v in zip(gradients, variables):
             # if "_w" not in v.name and "_dew" not in v.name:
             #     continue
